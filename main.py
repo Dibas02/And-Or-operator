63a76c4dbdad4e56565abf4c5d9c9234fd94c381
Dibas Talukder <debal1952@gmail.com>
2024-10-17 19:53:28 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+a=2
+b=1
+c=3
+
+if a<b and a<c:
+    print("a is the smallest number")
+
+else:
+    print("a is not the smallest number")
+
+
+if a<b or a<c:
+    print("a is not the largest number")
+
+else:
+    print("a is the largest number")
+
+
+
+st1 = "abc"
+st2 = "xyz"
+st3 = "abc"
+
+if st1 == st2 and st1 == st3 :
+    print("st1, st2 and st3 are all the same string")
+
+else:
+    print("st1, st2 and st3 are all not the same string")
+
+
+if st1 == st2 or st1 == st3:
+    print("There are at least 2 same/equal strings")
+
+else:
+    print("All of the strings ar not the same")
+
+
+
+
